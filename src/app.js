0797d88c94ce88059b74d1a43c27bb9f51c0dad4
thalisonCosta <thalisoncosta123@gmail.com>
2022-11-17 21:43:07 -0400

    :goal_net: catch erros if route not exists

--- src/app.js
+++ src/app.js
@@ -2,8 +2,28 @@ const express = require('express');
 const app = express();
 const productRouter = require('./routes/products-router');
 const ordersRouter = require('./routes/orders-router');
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
 
 app.use('/products', productRouter);
 app.use('/orders', ordersRouter);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+
 
 module.exports = app;
