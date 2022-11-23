a4efa5bc4287e3bfeec574812ac690d4fa60a22a
thalisonCosta <thalisoncosta123@gmail.com>
2022-11-22 21:19:09 -0400

    :passport_control: apply login as a middleware

--- src/routes/ordersRoute.js
+++ src/routes/ordersRoute.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const ordersController = require('../controllers/ordersController');
 const ordersMiddlewares = require('../middlewares/ordersMiddleware');
-
+const login = require('../middlewares/loginMiddleware');
 
 router.get('/', ordersController.allOrders);
 router.get('/:id', ordersController.oneOrder);
-router.post('/', ordersMiddlewares.validateProductId, ordersController.createOrder);
-router.delete('/:id', ordersController.deleteOrder);
+router.post('/', login, ordersMiddlewares.validateProductId, ordersController.createOrder);
+router.delete('/:id', login, ordersController.deleteOrder);
 
 module.exports = router;
--- src/routes/productsRoute.js
+++ src/routes/productsRoute.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const productsController = require('../controllers/productsController');
 const upload = require('../imageHandler');
-
+const login = require('../middlewares/loginMiddleware');
 
 router.get('/', productsController.allProducts);
 router.get('/:id', productsController.oneProduct);
-router.post('/', upload.single('productImage'), productsController.createProduct);
-router.patch('/:id', productsController.editProduct);
-router.delete('/:id', productsController.deleteProduct);
+router.post('/', login, upload.single('productImage'), productsController.createProduct);
+router.patch('/:id', login, productsController.editProduct);
+router.delete('/:id', login, productsController.deleteProduct);
 
 module.exports = router;
