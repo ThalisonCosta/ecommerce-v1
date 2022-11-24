f2c4fc570e81ed7438e72eaccf42b8b16fb3f7e4
thalisonCosta <thalisoncosta123@gmail.com>
2022-11-24 15:22:38 -0400

    :sparkles: also remove the image from the local storage

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -1,4 +1,5 @@
 const connection = require('./connection');
+const { unlink } = require('fs');
 
 const getAll = async () => {
   const [products] = await connection.execute('SELECT * FROM products');
@@ -39,6 +40,12 @@ const editProduct = async (id, product) => {
 };
 
 const deleteProduct = async (id) => {
+  const [deleteImage] = await connection.execute(`SELECT productImage FROM products WHERE productId = ${[id]}`);
+  const imageName = JSON.stringify(deleteImage).split('/')[1].split('"')[0];
+  unlink(`src/assets/${imageName}`, (err) => {
+    if (err) throw err;
+  });
+
   const query = 'DELETE FROM products WHERE productId = ?';
   const productDeleted = await connection.execute(query, [id]);
   return productDeleted;
