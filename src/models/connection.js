d089bfa14b1a03705e0df28c70bcb1b48185c0fb
thalisonCosta <thalisoncosta123@gmail.com>
2022-11-24 21:10:17 -0400

    :wrench: port to connect mysql

--- src/models/connection.js
+++ src/models/connection.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
+  port: process.env.PORT,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASS,
   database: process.env.MYSQL_DB
