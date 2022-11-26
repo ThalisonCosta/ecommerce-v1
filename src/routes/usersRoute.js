9fd66c07b2e219253a7bbd573388f68d0b9b6835
thalisonCosta <thalisoncosta123@gmail.com>
2022-11-26 11:38:15 -0400

    :art:

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const usersController = require('../controllers/usersController');
-const usersMiddlewares = require('../middlewares/usersModel');
+const usersMiddlewares = require('../middlewares/usersMiddleware');
 
 router.post('/create',
   usersMiddlewares.emailFormat,
