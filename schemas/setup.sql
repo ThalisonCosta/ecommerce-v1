4026a772f2f57346471678f840adfe1415b97d6a
thalisonCosta <thalisoncosta123@gmail.com>
2022-11-27 13:13:06 -0400

    :wrench: configurate docker

---
+++ schemas/setup.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE ecommerce;
+
+USE ecommerce
+CREATE TABLE
+    IF NOT EXISTS category(
+        categoryId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        categoryName VARCHAR(100)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS users (
+        userId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        userName VARCHAR(255) NOT NULL,
+        userEmail VARCHAR(255) NOT NULL,
+        userPass VARCHAR(255) NOT NULL
+    );
+
+CREATE TABLE
+    IF NOT EXISTS products (
+        productId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        categoryId INT NOT NULL,
+        productName VARCHAR(45) NOT NULL,
+        price FLOAT NOT NULL,
+        productImage VARCHAR(255),
+        productDescription VARCHAR(255),
+        FOREIGN KEY (categoryId) REFERENCES category (categoryId)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS orders(
+        orderId INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        quantity INT NOT NULL,
+        productId INT NOT NULL,
+        FOREIGN KEY (productId) REFERENCES products (productId)
+    );
