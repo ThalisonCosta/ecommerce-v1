d39a6381e8418e9604eda890d611464083a22396
thalisonCosta <thalisoncosta123@gmail.com>
2022-12-08 22:53:56 -0400

    :art:

--- src/services/imageHandler.js
+++ src/services/imageHandler.js
@@ -3,16 +3,16 @@ const path = require('path');
 const crypto = require('crypto');
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function (_req, _file, cb) {
     cb(null, './src/assets/');
   },
-  filename: function (req, file, cb) {
+  filename: function (_req, _file, cb) {
     cb(null, `${crypto.randomUUID()}.jpeg`);
   },
 
 });
 
-function filterFile(req, file, callback) {
+function filterFile(_req, file, callback) {
   const ext = path.extname(file.originalname);
   if (ext !== '.png' && ext !== '.jpg' && ext !== '.jpeg') {
     return callback(new Error('Only images are allowed'));
